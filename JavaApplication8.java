d36e801660925950d1135320687771c4aa41e9de
VassGyongyi <s.v.gyongyi@gmail.com>
2023-02-23 17:42:12 +0100

    java

---
+++ JavaApplication8.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author svgyo
+ */
+public class JavaApplication8 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    private static void elso10SzamOsszege(){
+        int osszeg =0;
+        for (int i = 0; i< 10; i++){
+            osszeg += i;}
+        return osszeg;  
+        
+    }
+    
+    private static void osszead(int a, int b){
+        return a+b;
+    }
+    private static void (int a, int b, int osszeg){
+        System.out.println("%d + %d = %d", a,b, osszeg);
+             
+    }
+}
