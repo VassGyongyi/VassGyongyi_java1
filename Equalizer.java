e6ed427b5b17075351f3cedf6040f57d7bef5bba
VassGyongyi <s.v.gyongyi@gmail.com>
2023-02-23 19:57:45 +0100

    túlterhelés

--- Equalizer.java
+++ Equalizer.java
@@ -8,6 +8,37 @@ package javaapplication8;
  *
  * @author svgyo
  */
-public class Equalizer {
+ public static void main(String[] args) {
+ public class Equalizer (){
+private static Random rnd = new Random();
+
+    
+   for (i = 0; i < 5; i++){
+     eq();}
     
 }
+private static void eq(){
+    int db = rnd.nextInt(3,8);
+    eq(db);
+}
+private static void eq(int hossz){
+    int db = rnd.nextInt(3,8);
+    String szin ="\u001B[45m";
+    for (i = 0; i < hossz; i++){
+        System.out.println(szin+" ");
+
+}
+private static void(boolean igaz, int hossz){
+    String szin ="\u001B[45m";
+    int db = rnd.nextInt(3,8);
+    if (hossz == 8):
+         System.out.println(szin+"("+hossz+")");
+    if (hossz == 4):
+         System.out.println(szin+"("+hossz+")");
+    else:
+        System.out.println(szin+" ");
+}
+
+}
+}
+
