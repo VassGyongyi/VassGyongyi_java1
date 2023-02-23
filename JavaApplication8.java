d7363bd1e7353b67fc3a61a0c14fdc78ed8bf010
VassGyongyi <s.v.gyongyi@gmail.com>
2023-02-23 18:37:06 +0100

    összeadás

--- JavaApplication8.java
+++ JavaApplication8.java
@@ -13,11 +13,22 @@ public class JavaApplication8 {
    
     public static void main(String[] args) {
         // TODO code application logic here
-        elso10SzamOsszege();
-        osszead(1,2);
+        //elso10SzamOsszege();
+        //osszead(1,2);
         kiir("Az elso 10 szám összege: "+ elso10SzamOsszege());
-        int szam1 = 7, szam2 = 4;
-        kiir("%d+%d=%d"+ osszead(szam1, szam2));
+        int[] szamok = {6,4,5,8};
+        ossz = osszead(szamok);
+        
+        int kimenet = 0;
+        int szam1 = 7, szam2 = 4, szam3 = 5, szam4 =9;
+        kimenet = osszead(szam1, szam2);
+        kimenet = osszead(kimenet+ szam3);
+        kimenet = osszead(kimenet + szam4);
+        kiir(Integer.toString(kimenet));
+       
+            
+        }
+        //kiir("%d+%d=%d"+ osszead(szam1, szam2));
         
     }
     private static int elso10SzamOsszege(){
@@ -28,7 +39,7 @@ public class JavaApplication8 {
         
     }
     
-    private static int osszead(int a, int b){
+    private static int osszead(int [] szamok){
         return a+b;
     }
     private static void kiir(String szoveg){
